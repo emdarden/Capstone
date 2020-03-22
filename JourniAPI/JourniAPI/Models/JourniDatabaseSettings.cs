3780731473a3fa81259801280eef90a1d228b3ed
emdarden <emdarden@umd.edu>
2020-03-21 21:15:07 -0500

    reverted database settings

--- JourniAPI/JourniAPI/Models/JourniDatabaseSettings.cs
+++ JourniAPI/JourniAPI/Models/JourniDatabaseSettings.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 namespace JourniAPI.Models
 {
-    public class JourniDatabaseSettings
+    public class JourniDatabaseSettings : IJourniDatabaseSettings
     {
-        public JourniDatabaseSettings()
-        {
-        }
+        public string TripsCollectionName { get; set; }
+        public string UsersCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+       
+    }
+
+    public interface IJourniDatabaseSettings
+    {
+        string TripsCollectionName { get; set; }
+        string UsersCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
     }
 }
