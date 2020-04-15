2861624b44ff3464ddb2eb3fea7a561308d2052f
Erika <emdarden@umd.edu>
2020-04-14 22:11:00 -0500

    capitalize query

--- JourniUI/src/app/components/search-bar/search-bar.component.ts
+++ JourniUI/src/app/components/search-bar/search-bar.component.ts
@@ -16,8 +16,15 @@ export class SearchBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
- getSearchResults(query){
-    this.router.navigate(['/search'], { queryParams: {query: query}});
+  getSearchResults(query){
+    this.router.navigate(['/search'], { queryParams: {query: this.titleCase(query)}});
   }
 
+  titleCase(str) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    return splitStr.join(' '); 
+ }
 }
