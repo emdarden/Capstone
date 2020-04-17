1eba263bf43a91e6a79b687013c12a69c3f5e21a
Erika <emdarden@umd.edu>
2020-04-17 00:30:37 -0500

    JRN-12 - added backend authentication

--- JourniAPI/JourniAPI/Controllers/TripsController.cs
+++ JourniAPI/JourniAPI/Controllers/TripsController.cs
@@ -3,6 +3,8 @@ using JourniAPI.Services;
 using JourniAPI.Models;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace JourniAPI.Controllers
 {
@@ -15,29 +17,38 @@ namespace JourniAPI.Controllers
         public TripsController(TripService tripService)
         {
             _tripService = tripService;
+
         }
 
+        [Authorize]
         [HttpGet]
-        public ActionResult<List<Trip>> GetAllTrips(string id)
+        public ActionResult<List<Trip>> GetAllTrips()
         {
+            string id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             return _tripService.GetAllTrips(id);
         }
 
+        [Authorize]
         [HttpGet("{tripName}")]
-        public ActionResult<Trip> GetTrip(string id, string tripName)
+        public ActionResult<Trip> GetTrip(string tripName)
         {
+            string id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             return _tripService.GetTrip(id, tripName);
         }
 
+        [Authorize]
         [HttpPost("{trip.tripName}")]
-        public ActionResult<Trip> CreateTrip(string id, Trip trip)
+        public ActionResult<Trip> CreateTrip(Trip trip)
         {
+            string id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             return Ok(_tripService.CreateTrip(id, trip));
         }
 
+        [Authorize]
         [HttpDelete("{trip.tripName}")]
-        public IActionResult RemoveTrip(string id, string tripName)
+        public IActionResult RemoveTrip(string tripName)
         {
+            string id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             _tripService.RemoveTrip(id, tripName);
             return NoContent();
         }
--- JourniAPI/JourniAPI/HasScopeRequirement.cs
+++ JourniAPI/JourniAPI/HasScopeRequirement.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using Microsoft.AspNetCore.Authorization;
+
 namespace JourniAPI
 {
-    public class HasScopeRequirement
+    public class HasScopeRequirement : IAuthorizationRequirement
     {
-        public HasScopeRequirement()
+        public string Issuer { get; }
+        public string Scope { get; }
+
+        public HasScopeRequirement(string scope, string issuer)
         {
+            Scope = scope ?? throw new ArgumentNullException(nameof(scope));
+            Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer));
         }
     }
 }
